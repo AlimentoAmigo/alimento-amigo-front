6b5222aef3170d0672d56161d98909347ecb79ad
Nicole Valletta <nicolelouisev@gmail.com>
2024-03-01 14:49:08 -0300

    Alterado Service e ListaProdutos para permitir acesso a lista sem necessidade de login

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -20,17 +20,22 @@ export const buscar = async(url: string, setDados: Function, header: Object) => 
     const resposta = await api.get(url, header)
     setDados(resposta.data)
   }
-  
-  export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-    const resposta = await api.post(url, dados, header)
-    setDados(resposta.data)
-  }
-  
-  export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-    const resposta = await api.put(url, dados, header)
-    setDados(resposta.data)
-  }
-  
-  export const deletar = async(url: string, header: Object) => {
-    await api.delete(url, header)
-  }
+
+export const buscarProduto = async(url: string, setDados: Function) => {
+  const resposta = await api.get(url)
+  setDados(resposta.data)
+}
+
+export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.post(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.put(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const deletar = async(url: string, header: Object) => {
+  await api.delete(url, header)
+}
